a01935ed36c4f2a253f41e2fc4807336394db806
Youssef Gamil <422328+yoga1290@users.noreply.github.com>
2024-03-19 21:23:52 -0700

    gh-actions

--- .github/workflows/build-ghpage.sh
+++ .github/workflows/build-ghpage.sh
@@ -1,13 +1,2 @@
-mkdir ./tmp;
-cd ./tmp;
-
-git clone https://$REPO_USERNAME:$GITHUB_TOKEN@$REPO_URL app #TODO
-cd ./app;
-
-TAG=$(date +%y.%m).$(git tag -l | wc -l)
-
-npm i
+npm i;
 npm run build;
-git add docs;
-git commit -m "${TAG}";
-git push;
