bff3e26c1166cc5dd2fd5406daecceb6ee5e4ecb
Youssef Gamil <yoga1290@gmail.com>
2025-09-23 14:39:21 -0700

    adds connect ui

--- src/components/pages/connect/index.tsx
+++ src/components/pages/connect/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import Card from 'yoga1290-ui-pool/react/card-with-icon'
+import Card from 'yoga1290-ui-pool/react/card-with-icon';
+import './style.scss';
 
 const emailMe = ()=>(location.href="mailto:yoga1290+ghpage@gmail.com");
 
 export default () => (
-    <div className="contact animate__animated animate__fadeInUp col-12 col-sm-6 col-md-4 col-lg-3">
-        <Card
-            title="Email me"
-            icon='mail'
-            click={emailMe} />
+    <div className="contact animate__animated animate__fadeInUp col-12 d-flex justify-content-center">
+        <div className='col-12 col-sm-6 col-md-4 col-lg-3'>
+            <Card
+                title="Email me"
+                icon='mail'
+                click={emailMe} />
+        </div>
     </div>
 )
