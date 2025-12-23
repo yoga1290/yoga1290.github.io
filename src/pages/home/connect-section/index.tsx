50bb2fff8b30c3da92bce6303b0d63ce772b8ed3
Youssef Gamil <yoga1290@gmail.com>
2025-12-23 15:20:04 -0800

    adds github button

--- src/pages/home/connect-section/index.tsx
+++ src/pages/home/connect-section/index.tsx
@@ -4,6 +4,16 @@ import './style.scss';
 
 const emailMe = ()=>(location.href="mailto:yoga1290+ghpage@gmail.com");
 
+const renderGitHubButton = () => (
+    <div className='gh-follow-bottn d-inline-flex col-12 justify-content-center'>
+        <a className="github-button"
+            href="https://github.com/yoga1290"
+            data-color-scheme="no-preference: light; light: light_high_contrast; dark: dark;"
+            data-size="large"
+            data-show-count="true"
+            aria-label="Follow @yoga1290 on GitHub">Follow @yoga1290</a> 
+    </div>);
+
 export default () => (
 
     <div className='connect-section col-12 px-0 justify-content-center row'>
@@ -21,8 +31,9 @@ export default () => (
                     </div>
                 </div>
     
+                {renderGitHubButton()}
             
-                <div className="contact animate__animated animate__fadeInUp col-12 d-flex justify-content-center">
+                <div className="contact animate__animated animate__fadeInUp col-12 d-flex justify-content-center">                    
                     <div className='col-12 col-sm-6 col-md-4 col-lg-3'>
                         <Card
                             title="Email me"
