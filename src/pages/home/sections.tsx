aed244888c4a11ade99322471a5a670f6cd25fe4
Youssef Gamil <yoga1290@gmail.com>
2026-06-23 17:20:14 -0400

    adds pipeline style

--- src/pages/home/sections.tsx
+++ src/pages/home/sections.tsx
@@ -12,12 +12,16 @@ const filterBySection = (sectionName: string) => (
                                 ({ section }:any) => (section == sectionName ) );
 
 const mapToSectionEl = ({ icon, title, section }: Metadata) => (<>
-    <div className={`${section}-section col-12 px-0`}>
-        <Section
-            data={jsonData.filter(filterBySection(section))}
-            icon={icon}
-            title={title}
-        />
+    <div className={`${section}-section col-12 p-0 m-0 pipeline-roadmap`}>
+        <span className="pipeline-roadmap__pin" />
+
+        <div className="pipeline-roadmap__content">
+            <Section
+                data={jsonData.filter(filterBySection(section))}
+                icon={icon}
+                title={title}
+            />
+        </div>
     </div>
 </>);
 
